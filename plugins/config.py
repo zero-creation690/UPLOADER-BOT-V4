44ae7e5ef304188425d42d3b5c2d39ca2a2f9836
zero-creation690 <zerocration2128@gmail.com>
2025-11-10 23:12:34 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -11,45 +11,43 @@ logging.basicConfig(
 
 class Config(object):
     
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", ))
-    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = "8062010233:AAExAW3Z-kpT17OTUXg0GQkCVsc7qnDUbXQ"
+    API_ID = 20288994
+    API_HASH = "d702614912f1ad370a0d18786002adbf"
     
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     MAX_FILE_SIZE = 2194304000
     TG_MAX_FILE_SIZE = 2194304000
     FREE_USER_MAX_FILE_SIZE = 2194304000
-    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
-    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
-    HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
+    CHUNK_SIZE = 128
+    DEF_THUMB_NAIL_VID_S = "https://placehold.it/90x90"
+    HTTP_PROXY = ""
     
     OUO_IO_API_KEY = ""
     MAX_MESSAGE_LENGTH = 4096
     PROCESS_MAX_TIMEOUT = 3600
     DEF_WATER_MARK_FILE = "@UploaderXNTBot"
 
-    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
+    BANNED_USERS = set()
 
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = "mongodb+srv://moviedatabase:venura%408907@cluster0.hg0etvt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    LOG_CHANNEL = -1002897456594
     LOGGER = logging
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = 8304706556
     SESSION_NAME = "UploaderXNTBot"
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+    UPDATES_CHANNEL = ""
 
     TG_MIN_FILE_SIZE = 2194304000
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    BOT_USERNAME = "@Urluploader_z_bot"
     ADL_BOT_RQ = {}
 
     # Set False off else True
-    TRUE_OR_FALSE = os.environ.get("TRUE_OR_FALSE", "").lower() == "true"
+    TRUE_OR_FALSE = False
 
     # Shortlink settings
-    SHORT_DOMAIN = environ.get("SHORT_DOMAIN", "")
-    SHORT_API = environ.get("SHORT_API", "")
+    SHORT_DOMAIN = "omegalinks.in"
+    SHORT_API = "a7ac9b3012c67d7491414cf272d82593c75f6cbb"
 
     # Verification video link
-    VERIFICATION = os.environ.get("VERIFICATION", "")
-
-    
+    VERIFICATION = ""
